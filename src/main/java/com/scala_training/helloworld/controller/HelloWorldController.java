f5208990a63299d92f754840b7bf716a588b42e9
Besay Montesdeoca <b.montesdeoca@indizen.com>
2025-04-29 15:32:16 +0200

    Fix code

--- src/main/java/com/scala_training/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/scala_training/helloworld/controller/HelloWorldController.java
@@ -7,22 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    // @GetMapping(path = {"/hello", "/user/{message}"})
-    // public String sendGreetings(@PathVariable(required=false,name="message") String message = null) {
-    //     if (message != null) {
-    //         return "Hello, " + message;
-    //     }
-    //     else { 
-    //         return "Hello"; 
-    //     }
-    // }
-
     @GetMapping("/hello")
     public String sendGreetings() {
-        if ("1".equals("1")) {
-            return "Hello World!";
-        } else {return "Hello World!";}
-        
-        
+        return "Hello World!";
     }
 }
